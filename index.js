cc988e692f57be56d9189b4700870209d06f5711
denissoboslai13 <soboslaid@gmail.com>
2026-01-19 10:58:00 +0100

    works on phone

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 const yesBtn = document.getElementById('yesBtn')
 const noBtn = document.getElementById('noBtn')
+let w = window.innerWidth;
+let h = window.innerHeight;
+
+console.log(w)
+if (w<600) {
+    var gap = 15
+} else {
+    var gap = 70
+}
 let startH = 2
 let startW = 3
 let noHeight = 75
 let noWidth = 85
 let yesFont = 35
 let noFont = 35
-let gap = 70
 let delVal = 0
+
+var x = window.matchMedia("(max-width: 600px)")
+
+const btnContainer = document.getElementById('btnContainer')
 
 document.addEventListener("DOMContentLoaded", () => {
   localStorage.removeItem("clickVal");
 });
 
 noBtn.addEventListener("click" ,() => {
-    startH += 1.5
-    startW += 3
-    noHeight -= 5
-    noWidth -= 6
-    yesFont += 6.5
-    noFont -= 2.5
-    gap -= 5
     delVal += 1
+
+    if (x.matches) {
+        startH += 1
+        startW += 1.5
+        noHeight -= 3
+        noWidth -= 4
+        yesFont += 3.5
+        noFont -= 2
+        gap -= 2
+        btnContainer.style.gap = `${gap}vh`;
+    } else {
+        gap -=5
+        startH += 1.5
+        startW += 3
+        noHeight -= 5
+        noWidth -= 6
+        yesFont += 6.5
+        noFont -= 2.5
+    }
 
     localStorage.setItem("clickVal", delVal)
 
